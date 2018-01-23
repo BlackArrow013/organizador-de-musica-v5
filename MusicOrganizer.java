eda93ec64950071c7b2ff50b5f2aa487d355c063
Jorge Jaular Lasaga <jordijaulas13@gmail.com>
2018-01-23 16:37:22 +0100

    El método findInTitle ha sido agregado a la clase MusicOrganizer

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -170,4 +170,18 @@ public class MusicOrganizer
             addTrack(track);
         }
     }
+    
+    /**
+     * Enumerar todas las pistas que contengan la cadena de búsqueda.
+     * @param searchString La cadena de búsqueda que hay que encontrar.
+     */
+    public void findInTitle(String searchString)
+    {
+        for(Track track : tracks) {
+            String title = track.getTitle();
+            if(title.contains(searchString)) {
+                System.out.println(track.getDetails());
+            }
+        }
+    }
 }
